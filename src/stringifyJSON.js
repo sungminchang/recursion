629c6294d49db12ad47b0eb4b4107c2134379f4c
SungMin <4sungmin@gmail.com>
2015-01-09 15:36:05 -0800

    This commit has a ton of alert invokations
    Used the alert invokations to debug my code. I'm about to take out the
    alert calls and fix the next bug in my code. I still need to make this
    solution recursive.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,67 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+	if (obj === null) {
+		return 'null';
+	}
+
+	if (typeof obj === "string") {
+		var result = '';
+		result += '"' + obj + '"';
+		return result;
+	}
+
+	if (typeof obj !== "object") {
+		return obj.toString();
+	};
+
+	if (Array.isArray(obj)) {
+		var result = obj.reduce(function(JSON, curr, i, arr) {
+			if (typeof curr === undefined ||
+				typeof curr === "function" ||
+				typeof curr === "symbol") {
+			} else if (typeof curr === "string") {
+				JSON += '"' + curr + '"' + ',';
+			} else {
+				JSON += curr + ',';
+				alert(typeof curr + ": " + curr);
+			}
+
+			return JSON;
+
+		}, '[');
+
+		if (result === '[') {
+			return '[]';
+		}
+		alert(result);
+		result = result.slice(0, result.length - 1);
+		result += ']';
+		alert(result + "executed");
+		return result;
+	}
+
+	if (typeof obj === "object") {
+
+		var result = obj.reduce(function(JSON, curr, i, arr) {
+			JSON += curr;
+			return JSON;
+		}, '{');
+
+		for (var key in obj) {
+			result += '"' + key + '"' + ':' + obj[key] + ',' ;
+		}
+
+		if (result === '{') {
+			return '{}';
+		}
+
+		result = result.slice(0, result.length - 1);
+		result += '}';
+		alert(result);
+
+		return result;
+	}
+
   // your code goes here
 };
